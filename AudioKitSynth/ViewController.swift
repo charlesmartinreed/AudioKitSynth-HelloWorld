225d22328fb9a965863856f5123d3c33bec072e5
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-19 11:16:58 -0500

    Initial Commit

---
+++ AudioKitSynth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioKitSynth
+//
+//  Created by Charles Martin Reed on 9/19/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
