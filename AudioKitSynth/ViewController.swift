d1e1cb12ecbd8bfa133073732eeb833ccf6f93ee
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-21 00:10:06 -0500

    Fix depreciated random double gen code.

--- AudioKitSynth/ViewController.swift
+++ AudioKitSynth/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //hook up the oscillators to the speakers and start the AK engine
+        //MARK:- hook up the oscillators to the speakers and start the AK engine
         AudioKit.output = AKMixer(oscillator, oscillator2)
         
         //using try optionally here becauase this throws but I don't need to handle an potential errors at this point
@@ -27,12 +27,15 @@ class ViewController: UIViewController {
     
     
     @IBAction func toggleSound(_ sender: UIButton) {
+        //MARK:- if the oscillator is playing, turn it off. If it's not playing, start it up and display the Hz in the button.
         if oscillator.isPlaying {
             oscillator.stop()
             sender.setTitle("Play Sine Wave", for: .normal)
         } else {
-            oscillator.amplitude = random(0.5, 1)
-            oscillator.frequency = random(220, 880)
+            //random(in:) replaces the depreciated random func from AudioKit
+            oscillator.amplitude = random(in: ClosedRange<Double>(uncheckedBounds: (lower: 0.5, upper: 1)))
+            oscillator.frequency = random(in: ClosedRange<Double>(uncheckedBounds: (lower: 220, upper: 880)))
+            
             oscillator.start()
             sender.setTitle("Stop Sine Wave at \(Int(oscillator.frequency))Hz", for: .normal)
         }
